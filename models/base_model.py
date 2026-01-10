095b27508076a50edfd12079923f0d233aebf4bc
EKIRABO <e.kirabo@alustudent.com>
2026-01-10 15:29:22 +0200

    fix base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -11,21 +11,20 @@ from models import storage
 class BaseModel:
     """BaseModel class defines all common attributes for other classes."""
     
-    def __init__(self, *args, **kwargs):
-        """Initialises a new BaseModel instance."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                if key in ["created_at", "updated_at"]:  # Fixed: separate strings
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-        
-        storage.new(self)  # Moved outside: always register with storage
+   def __init__(self, *args, **kwargs):
+    """Initialises a new BaseModel instance."""
+    if kwargs:
+        for key, value in kwargs.items():
+            if key == "__class__":
+                continue
+            if key in ["created_at", "updated_at"]:
+                value = datetime.fromisoformat(value)
+            setattr(self, key, value)
+    else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        storage.new(self) 
 
     def save(self):
         """Update updated_at and saves the instance"""
