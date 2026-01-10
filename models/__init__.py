9149a5f28f0f3b0a0a0d22351bbf09a88912e236
mufaro-k07 <m.kunze@alustudent.com>
2026-01-10 14:51:08 +0200

    __init__ added

---
+++ models/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Models package initializer
+"""
+class StorageStub:
+    """Temporary storage stub"""
+
+    def new(self, obj):
+        """Stub method"""
+        pass
+
+    def save(self):
+        """Stub method"""
+        pass
+
+
+storage = StorageStub()
--- models/base_model.py
+++ models/base_model.py
@@ -32,4 +32,9 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        """Returns a diction"""
+        """Return a dictionary representation of the instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
